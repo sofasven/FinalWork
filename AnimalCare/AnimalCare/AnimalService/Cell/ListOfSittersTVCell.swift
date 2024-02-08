ab10fa58e8dd54d8400410b6cabc1914896802a6
Sofa <sofasven00@gmail.com>
2024-02-08 16:00:15 +0300

    Onboarding cerated

--- AnimalCare/AnimalCare/AnimalService/Cell/ListOfSittersTVCell.swift
+++ AnimalCare/AnimalCare/AnimalService/Cell/ListOfSittersTVCell.swift
@@ -17,13 +17,10 @@ class ListOfSittersTVCell: UITableViewCell {
     @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
---
+++ AnimalCare/AnimalCare/Extensions/OnbordingVCExt.swift
@@ -0,0 +1,30 @@
+//
+//  OnbordingVCExt.swift
+//  AnimalCare
+//
+//  Created by Sofa on 8.02.24.
+//
+
+import UIKit
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+    }
+    
+
+}
